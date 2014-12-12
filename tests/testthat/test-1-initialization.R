8ff3832d8b1b33d96ac83be5ca8364256cd9ab4c
eyedvabny <eugene@yedvabny.com>
2014-12-11 20:37:23 -0800

    Fix the name change from init to initialize

--- tests/testthat/test-1-initialization.R
+++ tests/testthat/test-1-initialization.R
@@ -1,13 +1,13 @@
-context("Test init()")
+context("Test initialization()")
 
 test_that("Errors on incorrect chromosome length",{
-  expect_error(init(),'argument "C" is missing, with no default')
-  expect_error(init(-1),"invalid 'length' argument")
-  expect_error(init("1"),'non-numeric argument to binary operator')
+  expect_error(initialization(),'argument "C" is missing, with no default')
+  expect_error(initialization(-1),"invalid 'length' argument")
+  expect_error(initialization("1"),'non-numeric argument to binary operator')
 })
 
 test_that("Chromosomes only contain logical values",{
-  init_mat <- init(10)
+  init_mat <- initialization(10)
   init_unique <- unique(sort(init_mat))
 
   expect_equal(class(init_mat),"matrix")
@@ -19,14 +19,14 @@ test_that("Chromosomes only contain logical values",{
 })
 
 test_that("Correct dimensions when C<25",{
-  expect_null(dim(init(1)))
-  expect_equal(length(init(1)),2)
-  expect_equal(dim(init(10)),c(10,20))
-  expect_equal(dim(init(25)),c(25,50))
+  expect_null(dim(initialization(1)))
+  expect_equal(length(initialization(1)),2)
+  expect_equal(dim(initialization(10)),c(10,20))
+  expect_equal(dim(initialization(25)),c(25,50))
 })
 
 test_that("Correct dimensions when C>25",{
-  expect_equal(dim(init(26)),c(26,50))
-  expect_equal(dim(init(50)),c(50,50))
-  expect_equal(dim(init(100)),c(100,50))
+  expect_equal(dim(initialization(26)),c(26,50))
+  expect_equal(dim(initialization(50)),c(50,50))
+  expect_equal(dim(initialization(100)),c(100,50))
 })
